e5fdd55d947ab0d883e0d4776ec0136e245e770d
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-06-03 19:03:50 -0300

    Update useMoodTrendsData.ts

--- src/hooks/useMoodTrendsData.ts
+++ src/hooks/useMoodTrendsData.ts
@@ -4,20 +4,13 @@ import { useQuery } from '@tanstack/react-query';
 import { getMoodTrends } from '../api/moodsAPI';
 
 // CONSTANT
-const MOOD_TREND_QUERY_KEY = 'moodTrendData';
+import { MOOD_TREND_QUERY_KEY } from '../api/constants/moodTrendQueryKey';
 const userId = '6820e188c8970fadd5b3d4ce'; // TODO: Make me dynamic
-
-const fetchMoodTrendsData = async (timeframe: string) => {
-  const response = await getMoodTrends(userId, timeframe);
-
-  if (!(response.status === 200 || response.status === 201)) throw new Error('Network response was not ok');
-  return response.data;
-};
 
 const useMoodTrendsData = (timeframe: string) => {
   return useQuery({
     queryKey: [MOOD_TREND_QUERY_KEY, timeframe],
-    queryFn: () => fetchMoodTrendsData(timeframe),
+    queryFn: () => getMoodTrends(userId, timeframe),
     staleTime: 1000 * 60 * 5, // 5 minutes
   });
 };
