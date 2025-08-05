9cbdad71dd984796c4899208cd95927ad7059ba7
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-05 13:09:51 -0300

    add: manualChunks to vite.config.js for node deps

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      filename: "bundle-report.html"
+    }),
+  ],
   server: {
     host: true, // or host: "0.0.0.0"
     port: 5173, // or any other port
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-dom') || id.includes('react/jsx-runtime')) {
+              return 'vendor-react';
+            }
+            if (id.includes('@chakra-ui') || id.includes('@emotion')) {
+              return 'vendor-chakra';
+            }
+            if (id.includes('chart.js')) {
+              return 'vendor-charts';
+            }
+            if (id.includes('framer-motion')) {
+              return 'vendor-animations';
+            }
+            if (id.includes('date-fns')) {
+              return 'vendor-dates';
+            }
+
+            return 'vendor-other';
+          }
+        },
+      },
+    },
   },
 })
